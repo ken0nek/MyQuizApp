0ebc6f9cad34cf616d4742cb50371380ae9713a8
Ken Tominaga <ken.for.develop@gmail.com>
2014-06-07 13:06:18 +0900

    Initial Commit

---
+++ MyQuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyQuizApp
+//
+//  Created by Ken Tominaga on 2014/06/07.
+//  Copyright (c) 2014年 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
